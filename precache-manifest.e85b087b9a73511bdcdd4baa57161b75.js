d80670350d0a2b77d13783430fcc729cfe390b6b
Killbats <killbats@users.noreply.github.com>
2019-04-29 17:09:39 +0700

    Updates

---
+++ precache-manifest.e85b087b9a73511bdcdd4baa57161b75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efb572f07af95a48aede8cce268e413",
+    "url": "/mysf/index.html"
+  },
+  {
+    "revision": "85d90b1e899126c9f4d4",
+    "url": "/mysf/static/css/main.f4a53de9.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/mysf/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "85d90b1e899126c9f4d4",
+    "url": "/mysf/static/js/main.a4818916.chunk.js"
+  },
+  {
+    "revision": "9e9428815c1ae948ecd5",
+    "url": "/mysf/static/js/runtime~main.e40b3fd6.js"
+  },
+  {
+    "revision": "2928664fe1fc6aca88583a6f606d60ba",
+    "url": "/mysf/static/media/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.2928664f.svg"
+  },
+  {
+    "revision": "810041375d636c1c90bf9f54ad7df8c3",
+    "url": "/mysf/static/media/logo.81004137.png"
+  },
+  {
+    "revision": "4dc50a683a74b0728fc19920a5c203b7",
+    "url": "/mysf/static/media/rectangle.4dc50a68.png"
+  }
+]);
